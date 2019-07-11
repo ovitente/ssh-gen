e5a1eba7574be27deb7391f374b5052f0fca6f2a
Ivan Kostrubin <ikostrubin@infrapros.com>
2019-07-12 00:32:23 +0300

    testing structure2

--- struct2file.go
+++ struct2file.go
@@ -6,23 +6,25 @@ import (
 
 func main() {
 
+	type sshConfigOpts struct {
+		optName string
+		optValue string
+	}
+
 	type sshConfigBlock struct {
 		hostAlias    string
 		hostName     string
 		userName     string
 		portNumber   string
 		identityFile string
-		additionOpts string
+		additionOpts sshConfigOpts
 	}
 
-	type sshConfigOpts struct {
-		optName string
-		optValue string
-	}
 
-	element := sshConfigBlock{"Radas", "radas.gitlab.com", "bob", "223", "~/.ssh/work", "yes"}
+	confOpt := sshConfigOpts{"IdentitiesOnly", "yes"}
+	confElement := sshConfigBlock{"Radas", "radas.gitlab.com", "bob", "223", "~/.ssh/work", confOpt}
 
-	fmt.Printf("Host %s\n Hostname %s\n User %s\n Port %s\n IdentityFile %s\n IdentitiesOnly %s\n", element.hostAlias, element.hostName, element.userName, element.portNumber, element.identityFile, element.additionOpts)
+	fmt.Printf("Host %s\n Hostname %s\n User %s\n Port %s\n IdentityFile %s\n %s %s\n", confElement.hostAlias, confElement.hostName, confElement.userName, confElement.portNumber, confElement.identityFile, confOpt.optName, confOpt.optValue)
 // Todo: add additionOpts as integrated structure
 
 }
