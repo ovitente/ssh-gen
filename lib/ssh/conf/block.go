882b2b70400d0ed191389df75da6a3afbaa8fb3b
Ivan Kostrubin <ikostrubin@infrapros.com>
2019-07-19 01:22:15 +0300

    renamed strings

--- lib/ssh/conf/block.go
+++ lib/ssh/conf/block.go
@@ -21,17 +21,17 @@ func (b *Block) SetBlock(block []interface{}, additional []string) error {
 
 	b.HostAlias, ok = block[3].(string)
 	if !ok {
-		return errors.New("failed to convert project name into string")
+		return errors.New("failed to convert host alias into string")
 	}
 
 	b.HostName, ok = block[5].(string)
 	if !ok {
-		return errors.New("failed to convert project name into string")
+		return errors.New("failed to convert host name into string")
 	}
 
 	b.PortNumber, ok = block[6].(string)
 	if !ok {
-		return errors.New("failed to convert project name into string")
+		return errors.New("failed to convert port nunber into string")
 	}
 
 	b.AdditionalOptions = additional
